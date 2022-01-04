3013e3f746f2ce781c0610604546c1c23faa95fc
Ben Thorner <benthorner@users.noreply.github.com>
2022-01-04 22:09:05 +0000

    Remove calibration code from example
    This should be done separately in general e.g. in order to force a
    specific reference value for the CO2 sensor in fresh air conditions.

--- docs/examples/contrib.py
+++ docs/examples/contrib.py
@@ -1,8 +1,8 @@
 #! /usr/bin/python3
 
-import adafruit_bh1750
-import adafruit_scd30
 import board
+from adafruit_bh1750 import BH1750
+from adafruit_scd30 import SCD30
 from dotenv import load_dotenv
 
 from snsary import system
@@ -17,10 +17,6 @@ from snsary.sources import MultiSource
 from snsary.utils import configure_logging
 
 i2c = board.I2C()
-bh1750 = adafruit_bh1750.BH1750(i2c)
-scd30 = adafruit_scd30.SCD30(i2c)
-scd30.temperature_offset = 4
-
 load_dotenv()
 configure_logging()
 
@@ -28,8 +24,8 @@ MultiSource(
     OctopusSensor.from_env(),
     *AwairSensor.discover_from_env(),
     PyPMSSensor(sensor_name='PMSx003').stream.filter_names('pm10', 'pm25'),
-    AdafruitSensor(scd30).stream.filter_names('CO2', 'temperature', 'relative_humidity'),
-    AdafruitSensor(bh1750).stream.filter_names('lux')
+    AdafruitSensor(SCD30(i2c)).stream.filter_names('CO2', 'temperature', 'relative_humidity'),
+    AdafruitSensor(BH1750(i2c)).stream.filter_names('lux')
 ).stream.into(
     GraphiteOutput.from_env(),
     InfluxDBOutput.from_env()
