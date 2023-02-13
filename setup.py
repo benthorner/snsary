8930050ad5636c50b4d6ee4893bdc5f823bec1ba
Ben Thorner <benthorner@users.noreply.github.com>
2023-02-13 21:04:26 +0000

    Bump minimum package Python version to 3.9
    This matches the minimum tested version.

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setuptools.setup(
     ],
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
-    python_requires=">=3.8",
+    python_requires=">=3.9",
     install_requires=[
         "cachetools>=5.3.0",
         "wrapt>=1.14.1",
