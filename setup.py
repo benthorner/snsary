a4f0562cea1995ca0eb7c948a50cfb434dc17a35
Ben Thorner <benthorner@users.noreply.github.com>
2023-02-13 21:05:38 +0000

    Prepare setup.py for package publishing
    This matches setup.py for mock_serial [^1].
    
    [^1]: https://github.com/benthorner/mock_serial/blob/93e93e3a532570578ab9afa1058f70ae2cc19e9d/setup.py

--- setup.py
+++ setup.py
@@ -35,12 +35,17 @@ extras_require["all"] = [
     package for group in extras_require.values() for package in group
 ]
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
     name="snsary",
     version="0.0.1",
     author="Ben Thorner",
     author_email="benthorner@users.noreply.github.com",
     description="A framework for sensor metrics",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/benthorner/snsary",
     project_urls={
         "Bug Tracker": "https://github.com/benthorner/snsary/issues",
